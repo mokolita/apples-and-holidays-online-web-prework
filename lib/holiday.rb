8b2c538a204062e4c6701e38284b66beab22fcb2
Meg Galvez <meg@oakmaine.com>
2019-04-02 16:28:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,12 +77,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |season, holiday|
-    puts holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize! << ":"
-    holiday.each do |holiday, supplies|
+    holiday_hash.each do |season, holiday|
+      puts season.to_s.capitalize! << ":"
+      holiday.each do |holiday, supplies|
       supplies = supplies.join(", ")
-      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
+      holiday = holiday.to_s.split("_").collect {|w| w.capitalize}.join(" ")<< ":"
       puts "  " + holiday + " " + supplies
     end
   end
