7c7e212a0ace2cbfd17cdc0691b07cb1d7d6bcad
Meg Galvez <meg@oakmaine.com>
2019-04-02 15:29:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,6 +31,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
+  
+  add_supply_to_winter_holidays[:winter][:christmas] = (supply)
+  add_supply_to_winter_holidays[:winter][:new_years] = (supply)
 
 end
 
