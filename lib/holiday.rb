6bad1e7ba99306ab4ed3a7566c31e852eda8ff29
Meg Galvez <meg@oakmaine.com>
2019-04-02 16:34:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,8 +90,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
-  
+  holidays_with_bbq = ""
+  holiday_hash[:season][:holiday].collect do |holiday, item|
+    if item == "BBQ" 
+      
+      
 
 end
 
